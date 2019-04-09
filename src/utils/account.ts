6695964e0ab360b4a2602013c5dbb17a0689eec5
Johns Beharry <johns@peakshift.com>
2019-04-09 22:08:46 +0200

    refactor(account): define functions to generate and get keypairs from mnemonic

--- src/utils/account.ts
+++ src/utils/account.ts
@@ -13,7 +13,36 @@ class Account {
 	 */
 	constructor(network: Network) {
 		this.network = network;	
-	}
-	
-	
+	};
+
+	/*
+	 * Check if mnemonic is valid
+	 */
+	public isValid(mnomonic: string): boolean {
+		return;	
+	};
+
+	/*
+	 * Generate a mnemonic given some random entropy
+	 * @param {entropy} string - Entropy
+	 */
+	public generate(entropy: string): string {
+		return;
+	};
+
+	/*
+	 * Get public key from mnemonic
+	 * @param {mnemonic} string
+	 */
+	public getPrivateKey(): string {
+		return;
+	};
+
+	/*
+	 * Get public key from mnemonic
+	 * @param {mnemonic} string
+	 */
+	public getPublicKey(mnomonic: string): string {
+		return;
+	};
 }
